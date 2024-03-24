b2e9f205e1344a72d61d14ee1a717ad885b1d9b9
Ainus64 <ainumortis@gmail.com>
2024-03-24 19:14:22 +0100

    feat(cloudFlarePages): Configuration for cloudFlare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,47 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import cloudflare from "@astrojs/cloudflare";
 
-const LIVE_URL ="https://ainus64.github.io/";
+const SERVER_PORT = 4321;
+const LOCALHOST_URL = "";
+const LIVE_URL = "https://ainus64.github.io/";
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
 
-const BASE_URL = LIVE_URL;
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ainus64.github.io',
-	base: BASE_URL,
-	integrations: [
-		starlight({
-			title: 'Guia de Desarrollo Web',
-			social: {
-				github: 'https://github.com/ainus64/guia-de-desarrollo-web',
-			},
-			sidebar: [
-				{
-					label: 'git',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Comandos de Git', link: '/git/commands' },
-						{ label: 'Conventional Commits', link: '/git/commits' },
-					],
-				},
-				{
-					label: 'Astro',
-					autogenerate: { directory: 'astro' },
-				},
-			],
-		}),
-	],
+  site: 'https://ainus64.github.io',
+  base: BASE_URL,
+  output: 'server',
+  adapter: cloudflare({ mode: 'directory' }),
+  integrations: [starlight({
+    title: 'Guia de Desarrollo Web',
+    social: {
+      github: 'https://github.com/ainus64/guia-de-desarrollo-web'
+    },
+    sidebar: [{
+      label: 'git',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Comandos de Git',
+        link: '/git/commands'
+      }, {
+        label: 'Conventional Commits',
+        link: '/git/commits'
+      }]
+    }, {
+      label: 'Astro',
+      autogenerate: {
+        directory: 'astro'
+      }
+    }]
+  })],
+  adapter: cloudflare()
 });
