bc34d02d7bc522997ac7a1b0690b260230b449a5
Ainus64 <ainumortis@gmail.com>
2024-03-24 21:54:08 +0100

    fix(astro.config.mjs: fix para compilar el proyecto.

---
+++ .vercel/output/functions/_render.func/home/ainus/sources/web/guia-de-desarrollo-web/.vercel/output/_functions/chunks/generic_DmRsWf5G.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_DS8DPA0O.mjs').then(n => n.g);
+
+export { page };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,9 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-//import deno from '@astrojs/deno';
-// import cloudflare from "@astrojs/cloudflare";
 import vercel from "@astrojs/vercel/serverless";
-const LIVE_URL = "https://ainus64.github.io/";
-
-/*
-const SERVER_PORT = 4321;
-const LOCALHOST_URL = "";
-const LIVE_URL = "https://ainus64.github.io/";
-const SCRIPT = process.env.npm_lifecycle_script || "";
-const isBuild = SCRIPT.includes("astro build");
-
-let BASE_URL = LOCALHOST_URL;
-
-if (isBuild) {
-  BASE_URL = LIVE_URL;
-}
-*/
-
-let BASE_URL = LIVE_URL;
-cd 
-
 
 // https://astro.build/config
 export default defineConfig({
-  //site: 'https://ainus64.github.io',
-  //base: BASE_URL,
   output: 'server',
   adapter: vercel(),
   integrations: [starlight({
